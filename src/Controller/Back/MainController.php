3eb2dea98bcb55eb4c1149ed0137d40b06f3e458
Koné Trésor <103995919+TresorKone@users.noreply.github.com>
2022-10-29 06:23:37 +0200

    set up first template

---
+++ src/Controller/Back/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Back;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/back/home', name: 'app_back_home')]
+    public function index(): Response
+    {
+        return $this->render('back/main/home.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
